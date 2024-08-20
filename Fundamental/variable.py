e7ddcdff728b5ea17cbffc23d8f89c15c68373d8
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-20 15:01:54 +0600

    Add variable.py in Fundamental

---
+++ Fundamental/variable.py
@@ -0,0 +1,18 @@
+num = 30
+flotnum = 30.45
+doublenum = 30.459877556
+str1 = "30" #Immutable datatype
+flag = True
+print(num)
+print(flotnum)
+print(doublenum)
+print(str1)
+print(flag)
+num = 40
+flotnum = 40.45
+doublenum = 40.459877556
+flag = False
+print(num)
+print(flotnum)
+print(doublenum)
+print(flag)
