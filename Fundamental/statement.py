c41b69da1b02252f58d6081ce7dac8a21a09fc9b
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-21 10:59:25 +0600

    Add Conditional Statement in Fundamental

---
+++ Fundamental/statement.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+if num1 > num2:
+  print("First is greater")
+
+elif num1 < num2:
+  print("Second is greater")
+
+else:
+  print("The numbers are equal")
