4cbfb6e006f73dc7e63eec9d83fd9e2528546c8d
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-22 22:50:58 +0600

    Add UserInput in Fundamentals

---
+++ Fundamentals/userInput.py
@@ -0,0 +1,7 @@
+word : str = input("Enter a String : ")
+num : int = input("Enter a number : ")
+cgpa : float = input("Enter your cgpa : ")
+
+print(f"""The word is : {word}
+The Number is : {num}
+Your Cgpa is : {cgpa}""")
