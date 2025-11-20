e407982cebf06c522ff574da423dff16aaf9ab51
mostofa89 <mostofakamaljoy89@gmail.com>
2025-11-20 20:53:02 +0600

    Add Tuple Methods

---
+++ Fundamentals/Tuple/count.py
@@ -0,0 +1,9 @@
+nums : tuple = (1, 2, 3, 2, 4, 2, 5)
+print("Count of 2 in nums tuple:", nums.count(2))
+
+words : tuple = ("apple", "banana", "apple", "cherry", "banana", "apple")
+print("Count of 'apple' in words tuple:", words.count("apple"))
+
+colors : tuple = ("red", "blue", "green", "blue", "yellow", "blue")
+print("Count of 'blue' in colors tuple:", colors.count("blue"))
+
---
+++ Fundamentals/Tuple/index.py
@@ -0,0 +1,8 @@
+bikes : tuple = ("Yamaha", "Suzuki", "Kawasaki")
+print("Index of 'Suzuki' in bikes tuple:", bikes.index("Suzuki"))
+
+cars : tuple = ("Toyota", "Honda", "Ford", "Honda", "Chevrolet")
+print("Index of 'Honda' in cars tuple:", cars.index("Honda"))
+
+fruits : tuple = ("apple", "banana", "cherry", "date", "banana")
+print("Index of 'banana' in fruits tuple:", fruits.index("banana"))
