f06cb6b95ee79d2ed24b0bb2ec3ba444d33cf86d
mostofa89 <mostofakamaljoy89@gmail.com>
2025-11-20 21:29:13 +0600

    Add UserInput

---
+++ Fundamentals/UserInput/flaotInput.py
@@ -0,0 +1,3 @@
+cgpa : float = float(input("Enter your CGPA: "))
+
+print(f"Your CGPA is {cgpa:.2f}")
---
+++ Fundamentals/UserInput/intInput.py
@@ -0,0 +1,4 @@
+Age : int = int(input("Enter your age: "))
+
+print("You are " + str(Age) + " years old.")
+
