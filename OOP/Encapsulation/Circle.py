65fe0d587290b6a2705289e0ab5d4a8f0673e645
mostofa89 <mostofakamaljoy89@gmail.com>
2025-11-29 12:04:10 +0600

    Rename Area To Volume

--- OOP/Encapsulation/Circle.py
+++ OOP/Encapsulation/Circle.py
@@ -2,23 +2,23 @@ import math
 class Circle:
     def __init__(self, radius):
         self.__radius = radius
-        self.__area = 4/3*math.pi*radius**3
+        self.__vloume = 4/3*math.pi*radius**3
 
 
     def getRadius(self):
         return self.__radius
 
 
-    def area(self):
-        return self.__area
+    def volume(self):
+        return self.__vloume
 
 
 print("====================================")
 c1 = Circle(4)
 print("First circle radius:" , c1.getRadius())
-print("First circle area:" , c1.area())
+print("First circle area:" , c1.volume())
 print("====================================")
 c2 = Circle(5)
 print("Second circle radius:" , c2.getRadius())
-print("Second circle area:" , c2.area())
+print("Second circle area:" , c2.volume())
 print("====================================")
